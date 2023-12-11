7521bc84a40379392d9d1a996e47962838ca673d
Shama-David Grier-Johnson <sgrie10@wgu.edu>
2023-12-11 19:51:37 +0300

    Lab 02

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,25 +1,26 @@
 // import React from 'react';
-import Hornedbeast from "./Hornedbeast";
-import jsonData from "./data.json";
+import Hornedbeast from './Hornedbeast';
+import jsonData from './data.json';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function Gallery() {
-    const hornedBeasts = jsonData.map((beast) => (
-        <Hornedbeast 
-            key={beast._id} 
-            beast={{ 
-                title: beast.title, 
-                image_url: beast.image_url, 
-                description: beast.description 
-            }} 
-        />
-    ));
-
     return (
-        <div>
-            {hornedBeasts}
-        </div>
+        <Container>
+            <Row>
+                {jsonData.map((beast) => (
+                    <Col sm={12} md={6} lg={4} key={beast._id}>
+                        <Hornedbeast 
+                            beast={{ 
+                                title: beast.title, 
+                                image_url: beast.image_url, 
+                                description: beast.description 
+                            }} 
+                        />
+                    </Col>
+                ))}
+            </Row>
+        </Container>
     );
 }
 
 export default Gallery;
-
